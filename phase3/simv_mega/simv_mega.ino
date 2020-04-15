6444d62988c01142a30c386c46b2ca1439ce92fb
dispectra <42054890+dispectra@users.noreply.github.com>
2020-04-15 08:25:49 +0700

    Added & updae functions; Styling comments & desc.
    + Added digital filter function.
    + Updated send2Nano() to update2Nano() and inputted the real info to send.
    + Styling comments & description

--- phase3/simv_mega/simv_mega.ino
+++ phase3/simv_mega/simv_mega.ino
@@ -1,16 +1,73 @@
-/* Arduino Mega
- * Serial communication to/from Arduino Nano through Serial1
+/* Low Cost Ventilator - ITB
+ * This is the program for the main microcontroller (Arduino  \
+ * Mega). Read sensors, signaling motor controller (Arduino    \
+ * Nano), and communicate with HMI (Nextion).
  */
 
+//== LIBRARIES =============================================
+#include <SoftwareSerial.h>
+
+
+//== GLOBAL VARIABLES ======================================
+boolean status4Nano = 1;
+boolean warningVolume = 0;
+boolean warningPressure = 0;
+int volumeTidal = 500;
+float ERat = 0.2;
+int respirationRate = 12;
+boolean triggerInhale = 0;
+
+SoftwareSerial Serial2(2,3);
+
+
+//== MAIN SETUP ============================================
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(38400);
+  Serial1.begin(38400);
 }
 
+
+//== MAIN LOOP =============================================
 void loop() {
-  Serial.println("Write to Nano..."); Serial.flush();
-  Serial2.print("<02,04,1998>"); Serial2.flush();
-  Serial.println("Write to Nano...done"); Serial.flush();
-//  delay(10);
-  Serial.println("eol"); Serial.flush();
+  update2Nano();
+}
+
+
+//== FUNCTIONS =============================================
+
+//-- Sending necessary information to Arduino Nano ---------
+//-- (motor controller) through Serial2 port ---------------
+void update2Nano() {
+  String message = '<' + String(status4Nano) + ','
+                   + String(warningVolume) + ','
+                   + String(warningPressure) + ','
+                   + String(volumeTidal) + ','
+                   + String(ERat) + ','
+                   + String(respirationRate) + ','
+                   + String(triggerInhale) + '>';
+  Serial2.print(message); Serial2.flush();
+
+  // Debug message
+//  Serial.print(F("Message sent to Nano:\n\t")); Serial.print(message); Serial.flush();
+}
+
+//-- Digital Filter -----------------------------------------
+float signalFilter(float newImpulse) {
+// Variable declaration
+  byte filterOrder = 5;
+  float respons = 0.0;
+
+// Updating impulses
+  for (int i = 0; i<(filterOrder-1); i++) {
+    impulse[i] = impulse[i+1];
+  }
+  impulse[filterOrder]=newImpulse;
+
+// Calculating response
+  float weight[] = [0.1 0.2 0.3 0.2 0.1];
+  for (int i=0; i<ordeFilter; i++) {
+    respons += (blok[i] * weight[i]);
+  }
+
+  return respons;
 }
